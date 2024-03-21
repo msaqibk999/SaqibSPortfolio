6f6b9ec46da09866fe3d454e84805650c3ba2ed6
Saqib <tnluser@Ananthanarayanans-MacBook-Pro.local>
2024-03-21 18:32:00 +0530

    updated info

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,11 +11,11 @@ const experiences = [
     company: 'Byjus',
     url: 'https://byjus.com/',
     description: [
-      '<b>Fusion Proxy:</b> Engineered with Node.js and Express.js to optimize requisites (pre, post,test) for associated divisions such as Aakash, Byjus Neo, and Tutor Plus.',
+      '<b>Engineered Fusion Proxy:</b> Developed a Node.js and Express.js service in a 2-person team, consolidating requests across Aakash, BYJU\'S, and Tutor Plus,transitioning vertical infrastructure to a horizontal model to enhance efficiency.',
       'Coordinated concurrent requests to services, resulting in a reduction of overall response times from an average of 5 seconds to 2 seconds.',
       'Implemented io Redis to streamline multiple requests in a single connection, resulting in a 50% reduction in data retrieval response times from 3 seconds to 1.5 seconds.',
-      'Conducted thorough testing, debugging, and continuous maintenance to ensure sustained system reliability.',
-      '<b>K3 (Disney + Byju\'s):</b> Provided pivotal support for the backend infrastructure of K3 (Disney + Byju\'s), leveraging the robust Ruby on Rails framework.',
+      '<b>Activation Portal:</b> Developed and implemented a kit activation portal using ReactJS for the BYJU\'S learning app in the US, facilitating seamless activation through unique activation codes, contributing to an app with over 10 million downloads.',
+      '<b>BYJU\'S Early Learn:</b> Provided pivotal support for the backend infrastructure of BYJU\'S Early Learn:, leveraging the robust Ruby on Rails framework.',
       'Collaborated closely with the product team, implementing changes and enhancements to align the backend with the product vision.',
       '<b>Database Migration:</b> Executed a pivotal database migration initiative, resulting in substantial monthly savings of $500 for both K3India and K3 Global teams.'
     ]
@@ -29,6 +29,7 @@ const experiences = [
       'Built a CAT college predictor page, empowering users to accurately assess their eligibility for colleges based on academic performance, leading to a 25% boost in informed decision-making and user interaction.',
       '<b>Revamped Workshop Page:</b> Revamped Workshop Page for diverse states based on user login statuses and actions, achieving a notable 10% post-registration drop-off reduction with engaging hook implementation.',
       '<b>CSIR NET Eligibility Calculator:</b> Developed and integrated an eligibility calculator for lectureship and JRF, leading to a notable 25% increase in website traffic and an impressive 30% rise in signups.',
+      '<b>Unbounce Integration and Lead Generation:</b> Coded and deployed static pages on Unbounce, seamlessly integrating them into the website flow through JavaScript event dispatchers/listeners to optimize lead generation efforts.',
       '<b>SEO Enhancement and Web Vitals Optimization:</b> Enhanced SEO and optimized web vitals, focusing on improving CLS and other metrics to enhance website accessibility and performance, contributing to an overall improved user experience.'
     ]
   }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,9 @@ import {ReactComponent as NodeLogo} from '../media/skills/NodeLogo.svg'
 import {ReactComponent as HtmlLogo} from '../media/skills/HtmlLogo.svg'
 import {ReactComponent as CssLogo} from '../media/skills/CssLogo.svg'
 import {ReactComponent as JsLogo} from '../media/skills/JsLogo.svg'
+import {ReactComponent as MysqlLogo} from '../media/skills/MysqlLogo.svg'
 import {ReactComponent as PostgresLogo} from '../media/skills/PostgresLogo.svg'
+import {ReactComponent as MongodbLogo} from '../media/skills/MongodbLogo.svg'
 import {ReactComponent as RailsLogo} from '../media/skills/RailsLogo.svg'
 import {ReactComponent as JavaLogo} from '../media/skills/JavaLogo.svg'
 import {ReactComponent as AwsLogo} from '../media/skills/AwsLogo.svg'
@@ -21,14 +23,16 @@ export default function Skills() {
       <div className={styles.mainContainer} id='skills'>
         <h1 className={styles.heading}>MY SKILLS</h1>
         <div className={styles.subContainer}>
-          <CircularProgressBar progressEndValue={85} speed={10} skill={"ReactJS"}><ReactLogo className={styles.svg}/></CircularProgressBar>
-          <CircularProgressBar progressEndValue={75} speed={10} skill={"NodeJS"}><NodeLogo className={styles.svg}/></CircularProgressBar>
           <CircularProgressBar progressEndValue={95} speed={10} skill={"HTML"}><HtmlLogo className={styles.svg}/></CircularProgressBar>
           <CircularProgressBar progressEndValue={95} speed={10} skill={"CSS"}><CssLogo className={styles.svg}/></CircularProgressBar>
           <CircularProgressBar progressEndValue={95} speed={10} skill={"Javascript"}><JsLogo className={styles.svg}/></CircularProgressBar>
+          <CircularProgressBar progressEndValue={85} speed={10} skill={"ReactJS"}><ReactLogo className={styles.svg}/></CircularProgressBar>
+          <CircularProgressBar progressEndValue={80} speed={10} skill={"NodeJS"}><NodeLogo className={styles.svg}/></CircularProgressBar>
+          <CircularProgressBar progressEndValue={85} speed={10} skill={"MySQL"}><MysqlLogo className={styles.svg}/></CircularProgressBar>
           <CircularProgressBar progressEndValue={85} speed={10} skill={"PostgreSQL"}><PostgresLogo className={styles.svg}/></CircularProgressBar>
+          <CircularProgressBar progressEndValue={85} speed={10} skill={"MongoDB"}><MongodbLogo className={styles.svg}/></CircularProgressBar>
+          <CircularProgressBar progressEndValue={85} speed={10} skill={"Java"}><JavaLogo className={styles.svg}/></CircularProgressBar>
           <CircularProgressBar progressEndValue={70} speed={10} skill={"Rails"}><RailsLogo className={styles.svg}/></CircularProgressBar>
-          <CircularProgressBar progressEndValue={95} speed={10} skill={"Java"}><JavaLogo className={styles.svg}/></CircularProgressBar>
           <CircularProgressBar progressEndValue={70} speed={10} skill={"AWS"}><AwsLogo className={styles.svg}/></CircularProgressBar>
           <CircularProgressBar progressEndValue={70} speed={10} skill={"Docker"}><DockerLogo className={styles.svg}/></CircularProgressBar>
           <CircularProgressBar progressEndValue={95} speed={10} skill={"Git"}><GitLogo className={styles.svg}/></CircularProgressBar>
